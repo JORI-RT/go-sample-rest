0b19b2b4fa44285f0226176373ade5771ea13362
ryo.takaki <sigaryoutakaki@gmail.com>
2020-09-09 19:52:31 +0900

    add comment

--- stringsvc/instrumenting.go
+++ stringsvc/instrumenting.go
@@ -18,6 +18,8 @@ type instrumentingMiddleware struct {
 // 戻り値に名前をつけることで、naked returnできる
 // MW ... デコレータでいろいろやるらしい
 func (mw instrumentingMiddleware) Uppercase(s string) (output string, err error) {
+	// deferは渡した関数の実行を呼び出し元の関数が終わるまで遅延させる
+	// deferはLIFOで実行される　複数のデコレータはスタックされる
 	defer func(begin time.Time) {
 		lvs := []string{"method", "uppercase", "error", fmt.Sprint(err != nil)}
 		mw.requestCount.With(lvs...).Add(1)
